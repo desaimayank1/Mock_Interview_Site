ad1204c035b64da8c3155c750e4870f197a553ee
desaimayank1 <desaimayank478@gmail.com>
2024-10-28 21:21:54 +0530

    first commit

---
+++ Front_end/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Login from './components/Login.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+function App() {
+
+  return (
+    <>
+      <Header/>
+      {/* <Login/> */}
+      {/* <Footer/>  */}
+    </>
+  )
+}
+
+export default App
